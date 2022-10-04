d2fa6015047899e07ee0d8f887a4ec17077a269d
Renmy <renmyenriquez@gmail.com>
2022-10-04 00:11:11 -0400

    all good!!

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -12,9 +12,11 @@ class CardForm extends Component {
             formErrors: {cardname:'',cardnumber:'',expdate:'',cvc:''},
             cardnamevalid:false,
             cardnumbervalid:false,
+            expMvalid:false,
+            expYvalid:false,
             expdatevalid:false,
             cvcvalid:false,
-            formvalid:false
+            formValid:false
         }
     }
 
@@ -35,9 +37,10 @@ class CardForm extends Component {
     validateField = (fieldname, value) => {
         let cardnamevalid = this.state.cardnamevalid;
         let cardnumbervalid = this.state.cardnumbervalid;
-        let expdatevalid = this.state.expMvalid;
         let cvcvalid = this.state.cvcvalid;
         let formErrorField = this.state.formErrors;
+        let expMvalid = this.state.expMvalid;
+        let expYvalid = this.state.expYvalid;
 
         switch (fieldname) {
             case("cardname"): {
@@ -45,46 +48,75 @@ class CardForm extends Component {
                 formErrorField.cardname = cardnamevalid?'':'Only letters and more than 5 chars.';
                 break;
             } 
+
             case('cardnumber'): {
                 cardnumbervalid = ((/^([0-9]{16})$/).test(value));
                 formErrorField.cardnumber = cardnumbervalid?'':'Wrong format, only numbers and must be 16.';
                 break;
             }
+
+            case('expM'): {
+                if (value.length===0){
+                    formErrorField.expdate = 'Can`t be blank';
+                    break; 
+                }
+                expMvalid = value<13
+                formErrorField.expdate = (expMvalid && expYvalid)?'':'Wrong date format';
+                break;
+            }
+
+            case('expY'): {
+                if (value.length===0){
+                    formErrorField.expdate = 'Can`t be blank';
+                    break; 
+                }
+                expYvalid = value>22
+                formErrorField.expdate = (expMvalid && expYvalid)?'':'Wrong date format';
+                break;
+            }
+
             case('cvc'): {
                 cvcvalid = ((/^([0-9]{3})$/).test(value));
-                formErrorField = cvcvalid?'':'Wrong CVC format, must be 3 digits number. '
+                formErrorField.cvc = cvcvalid?'':'Wrong format, 3 digits number. '
+                break;
             }
-            default: {
-                if(value.length===0){
-                    formErrorField.expdate = 'Can`t be blank';
-                    expdatevalid = false;
-                    break;   
-                }
-                else if((fieldname==='expM' && value>12) || (fieldname==='expY' && value<22)) {
-                    formErrorField.expdate = 'Wrong date format.';
-                    expdatevalid = false;
-                    break;
-                } else {
-                    formErrorField.expdate = '';
-                    expdatevalid = true;
-                }
+
+            // default: {
+            //     if(value.length===0){
+            //         formErrorField.expdate = 'Can`t be blank';
+            //         expdatevalid = false;
+            //     }
+            //     if(fieldname=='expM' && value>12) {
+            //         formErrorField.expdate = 'Wrong date format.';
+            //         expMvalid = false;
+            //     }
+            //     else if(fieldname==='expY' && value<22){
+            //         formErrorField.expdate = 'Wrong date format.';
+            //         expYvalid = false;
+            //     } 
+            //     if(expYvalid && expMvalid) {
+            //         formErrorField.expdate = '';
+            //         expdatevalid = true;
+            //     }
+            //     console.log(expMvalid)
                 // formErrorField = value.length!==0?'':'Can`t be blank';
                 // break;
                 // formErrorField = 0<value<12?'':'Wrong date format.'
                 
             }
-        }
+        
         this.setState({ formErrors: formErrorField,  
                         cardnamevalid: cardnamevalid, 
                         cardnumbervalid: cardnumbervalid,
-                        expdatevalid: expdatevalid,
+                        expMvalid: expMvalid,
+                        expYvalid: expYvalid,
                         cvcvalid: cvcvalid
                         }, this.validateForm)
 
     }
 
     validateForm() {
-        this.setState({formValid: this.state.cardnamevalid && this.state.cardnumbervalid && this.state.expdatevalid  && this.state.cvcvalid });
+        this.setState({formValid: this.state.cardnamevalid && this.state.cardnumbervalid && this.state.expMvalid && this.state.expYvalid && this.state.cvcvalid });
       }
 
     render () {
@@ -115,18 +147,19 @@ class CardForm extends Component {
                             exp. date (mm/YY)
                         </label>
                         <div className="inline-flex" name="exp-date">
-                            <input className="appearance-none block w-1/2  text-gray-700 border border-gray-200 rounded py-2 px-4  focus:outline-none focus:bg-white focus:border-gray-500 mr-2" type="text" name='expM'placeholder="MM" maxLength={2} value={this.state.expM} onChange={this.handleUserInput} />
-                            <input className="appearance-none block w-1/2 text-gray-700 border border-gray-200 rounded py-2 px-4 focus:outline-none focus:bg-white focus:border-gray-500" type="text" name='expY'placeholder="YY" maxLength={2} value={this.state.expY} onChange={this.handleUserInput}/>
+                            <input className="appearance-none block w-1/2  text-gray-700 border border-gray-200 rounded py-2 px-4  focus:outline-none focus:bg-white focus:border-gray-500 mr-2" type="text" name='expM' placeholder="MM" maxLength={2} value={this.state.expM} onChange={this.handleUserInput} />
+                            <input className="appearance-none block w-1/2 text-gray-700 border border-gray-200 rounded py-2 px-4 focus:outline-none focus:bg-white focus:border-gray-500" type="text" name='expY' placeholder="YY" maxLength={2} value={this.state.expY} onChange={this.handleUserInput}/>
                         </div>
                         <small className="text-red-400 text-[11px]">{this.state.formErrors['expdate']}</small>
                     </div>
                     <div className="w-1/2 pr-3 mb-6 md:mb-0">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs mb-2" for="cvc ">cvc</label>
                         <input className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-2 px-4 focus:outline-none focus:bg-white focus:border-gray-500" name="cvc" type="text" placeholder="e.g 123" maxLength={3} value={this.state.cvc} onChange={this.handleUserInput}/>
+                        <small className="text-red-400 text-[11px]">{this.state.formErrors['cvc']}</small>
                     </div>
                 </div>
                 <div>
-                    <button type="submit" className="w-full text-center text-lg bg-[#21092F] rounded-md p-3 text-gray-200 font-grotesk" disabled={!this.state.formvalid}  >Confirm</button>
+                    <button type="submit" className="w-full text-center text-lg bg-[#21092F] rounded-md p-3 text-gray-200 font-grotesk disabled:cursor-not-allowed " disabled={!this.state.formValid}  >Confirm</button>
                 </div>
             </form>
         )
